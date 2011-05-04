fa2a0dc456c29a415a843dcb942bb09062e699e6
Nick Thomas <nick@bytemark.co.uk>
2011-05-04 14:47:19 +0100

    Allow Jetty's SSL settings to be configured through the handler

--- lib/rack/handler/jetty.rb
+++ lib/rack/handler/jetty.rb
@@ -6,27 +6,47 @@ module Rack
     class Jetty
       attr_reader :app
       attr_reader :options
-      
+
       def initialize(app, options={})
         @app = app
         @options = options
       end
-      
+
       def self.run(app, options = {})
         new(app,options).run()
       end
-      
+
       def run()
-        @connector = Java::org.mortbay.jetty.bio.SocketConnector.new
+        if options.has_key?(:ssl)
+          ssl = options[:ssl]
+          if ssl.is_a?(Hash) && ssl[:keystore] && ssl[:keystore_password]
+            @connector = Java::org.mortbay.jetty.security.SslSocketConnector.new
+            @connector.set_keystore(ssl[:keystore])
+            @connector.set_password(ssl[:keystore_password])
+            @connector.set_key_password(ssl[:key_password])
+            @connector.set_truststore(ssl[:truststore] || ssl[:keystore])
+            @connector.set_trust_password(
+              ssl[:truststore_password] || ssl[:keystore_password]
+            )
+          else
+            raise ArgumentError.new(
+              "SSL requested but keystore, keystore password or key password" + 
+              "not provided"
+            )
+          end
+        end
+
+        @connector ||= Java::org.mortbay.jetty.bio.SocketConnector.new
+
         @connector.set_host(options[:Host])
         @connector.set_port(options[:Port].to_i)
 
         @jetty = Java::org.mortbay.jetty.Server.new
         @jetty.addConnector(@connector)
-        
+
         bridge = RackJetty::ServletHandler.new
         bridge.handler = self
-        
+
         @jetty.set_handler(bridge)
         @jetty.start
       end
@@ -34,14 +54,16 @@ module Rack
       def running?
         @jetty && @jetty.is_started
       end
-      
+
       def stopped?
         !@jetty || @jetty.is_stopped
       end
-      
+
       def stop()
         @jetty && @jetty.stop
       end
+
     end
   end
 end
+
