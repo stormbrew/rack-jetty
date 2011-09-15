5211634969b9dfc2b7e7782d5bdb0ddc3d070ca2
Thomas Dudziak <thomas@ning.com>
2011-09-14 19:51:50 -0700

    Fixed bad imports

--- lib/rack/handler/jetty.rb
+++ lib/rack/handler/jetty.rb
@@ -1,5 +1,21 @@
 require 'rack_jetty/java_input'
 require 'rack_jetty/servlet_handler'
+require 'java'
+
+java_import 'org.mortbay.jetty.NCSARequestLog'
+java_import 'org.mortbay.jetty.Server'
+java_import 'org.mortbay.jetty.bio.SocketConnector'
+java_import 'org.mortbay.jetty.handler.DefaultHandler'
+java_import 'org.mortbay.jetty.handler.HandlerCollection'
+java_import 'org.mortbay.jetty.handler.ContextHandlerCollection'
+java_import 'org.mortbay.jetty.handler.RequestLogHandler'
+java_import 'org.mortbay.jetty.handler.StatisticsHandler'
+java_import 'org.mortbay.jetty.nio.SelectChannelConnector'
+java_import 'org.mortbay.jetty.security.SslSocketConnector'
+java_import 'org.mortbay.management.MBeanContainer'
+java_import 'org.mortbay.thread.QueuedThreadPool'
+java_import 'java.lang.management.ManagementFactory'
+
 
 module Rack
   module Handler
@@ -17,20 +33,23 @@ module Rack
       end
       
       def run()
-        thread_pool = Java::org.mortbay.thread.QueuedThreadPool.new
+        thread_pool = QueuedThreadPool.new
         thread_pool.setMinThreads((options[:Min_threads] || 10).to_i)
         thread_pool.setMaxThreads((options[:Max_threads] || 200).to_i)
         thread_pool.setLowThreads((options[:Low_threads] || 50).to_i)
         thread_pool.setSpawnOrShrinkAt(2)
 
-        connectors = []
+        @jetty = Java::org.mortbay.jetty.Server.new
+        @jetty.setThreadPool(thread_pool)
+        @jetty.setGracefulShutdown(1000)
+
         stats_on = options[:With_Stats] || true
 
         if options[:Use_NIO] || true
-          http_connector = Java::org.mortbay.jetty.nio.SelectChannelConnector.new
+          http_connector = SelectChannelConnector.new
           http_connector.setLowResourcesConnections(20000)
         else
-          http_connector = Java::org.mortbay.jetty.bio.SocketConnector.new
+          http_connector = SocketConnector.new
         end
         http_connector.setHost(options[:Host] || 'localhost')
         http_connector.setPort(options[:Port].to_i)
@@ -40,10 +59,10 @@ module Rack
         http_connector.setLowResourceMaxIdleTime(5000)
         http_connector.setAcceptQueueSize((options[:Accept_queue_size] || thread_pool.getMaxThreads).to_i)
         http_connector.setName("HttpListener")
-        connectors << http_connector
+        @jetty.addConnector(http_connector)
 
         if options[:Ssl_Port] && options[:Keystore] && options[:Key_password]
-          https_connector = Java::org.mortbay.jetty.security.SslSocketConnector.new
+          https_connector = SslSocketConnector.new
 
           https_connector.setKeystore(options[:Keystore])
           https_connector.setKeystoreType(options[:Keystore_type] || 'JKS')
@@ -56,54 +75,42 @@ module Rack
           https_connector.setLowResourceMaxIdleTime(5000)
           https_connector.setAcceptQueueSize(http_connector.getAcceptQueueSize)
           https_connector.setName("HttpsListener")
-          connectors << https_connector
+          @jetty.addConnector(http_connector)
         end
-
-        @jetty = Java::org.mortbay.jetty.Server.new
-        @jetty.setThreadPool(thread_pool)
-        @jetty.setConnectors(connectors)
-        @jetty.setGracefulShutdown(1000)
 
         bridge = RackJetty::ServletHandler.new
         bridge.handler = self
 
-        handlers = Java::org.mortbay.jetty.handler.HandlerCollection.new
-        context_handlers = Java::org.mortbay.jetty.handler.ContextHandlerCollection.new
-        root = Java::org.mortbay.jetty.servlet.Context(context_jandlers, "/", Java::org.mortbay.jetty.servlet.Context::NO_SESSIONS)
-
-        root.addFilter(Java::org.mortbay.jetty.servlet.FilterHolder.new(bridge), "/", 0)
-        root.addServlet(Java::org.mortbay.jetty.servlet.DefaultServlet.new, "/")
-        handlers.addHandler(context_handlers)
-        handlers.addHandler(Java::org.eclipse.jetty.server.handler.DefaultHandler.new)
+        handlers = HandlerCollection.new
+        handlers.addHandler(bridge)
 
         if options[:Request_log] || options[:Request_log_path]
-          request_log_handler = Java::org.mortbay.jetty.handler.RequestLogHandler.new
+          request_log_handler = RequestLogHandler.new
 
-          request_log_handler.setRequestLog(options[:Request_log] || Java::org.eclipse.jetty.server.NCSARequestLog.new(options[:Request_log_path]))
+          request_log_handler.setRequestLog(options[:Request_log] || NCSARequestLog.new(options[:Request_log_path]))
           handlers.addHandler(request_log_handler)
         end
         if stats_on
-          mbean_container = Java::javax.management.MBeanContainer.new(Java::java.lang.management.ManagementFactory.getPlatformMBeanServer)
+          mbean_container = MBeanContainer.new(ManagementFactory.getPlatformMBeanServer)
 
           @jetty.getContainer.addEventListener(mbean_container)
           mbean_container.start
 
-          stats_handler = Java::org.mortbay.jetty.handler.StatisticsHandler.new
+          stats_handler = StatisticsHandler.new
           stats_handler.addHandler(handlers)
-          server.addHandler(stats_handler)
+          @jetty.addHandler(stats_handler)
         else
-          server.addHandler(handlers)
+          @jetty.addHandler(handlers)
         end
-        puts "Starting jetty"
         @jetty.start
       end
       
       def running?
-        @jetty && @jetty.is_started
+        @jetty && @jetty.isStarted
       end
       
       def stopped?
-        !@jetty || @jetty.is_stopped
+        !@jetty || @jetty.isStopped
       end
       
       def stop()
